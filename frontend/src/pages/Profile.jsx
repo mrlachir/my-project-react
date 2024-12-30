a6eb7dea8d9f3a74c7d3d2f03c42759c29ad612b
Mohammed Lachir <lachirmr@gmail.com>
2024-12-30 22:21:46 +0100

    hh

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -6,15 +6,11 @@ const ProfilePage = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Check if there's a valid token in localStorage
     const token = localStorage.getItem("token");
 
     if (!token) {
-      // If no token, redirect to login page
       navigate("/login");
     } else {
-      // Optionally: Verify the token on the backend (by sending a request to a protected route)
-      // You can also store user details in localStorage during login for easy access
       const storedUser = JSON.parse(localStorage.getItem("user"));
       if (storedUser) {
         setUser(storedUser);
